1f19ef41c9d5e56e68c3a505c4d938dccb1a4f27
zkakaos0527-lgtm <zkakaos0527-lgtm@users.noreply.github.com>
2026-07-17 16:44:39 +0800

    Force dynamic notebook dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { HomeClient } from "./HomeClient";
 import { listNotebookCards } from "@/lib/supabase";
 import type { DashboardNotebookCard } from "@/types";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   let cards: DashboardNotebookCard[] = [];
   let error = false;
